e1880115ef5b352b1e1bdc7a39feefff8bc38b09
manuelsanchez2 <manusansan22@gmail.com>
2023-06-16 14:37:20 +0200

    Add favicon and og images

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -62,11 +62,55 @@ export default component$(() => {
 })
 
 export const head: DocumentHead = {
-  title: 'Capcom Advisor',
+  title: 'Unofficial Capcom Advisor',
   meta: [
     {
       name: 'description',
-      content: 'Qwik site description',
+      content: 'Find out which game is for you with this cool advisor!',
+    },
+    {
+      name: 'keywords',
+      content: 'capcom, game, steam, re4, monster hunter, Qwik',
+    },
+    {
+      name: 'author',
+      content: 'Manuel Sanchez',
+    },
+    {
+      property: 'og:title',
+      content: 'Unofficial Capcom Advisor',
+    },
+    {
+      property: 'og:description',
+      content: 'Find out which game is for you with this cool advisor!',
+    },
+    {
+      property: 'og:image',
+      content: 'https://capcom.vercel.app/thumbnail.png',
+    },
+    {
+      property: 'og:url',
+      content: 'https://capcom.vercel.app/',
+    },
+    {
+      name: 'twitter:card',
+      content: 'summary_large_image',
+    },
+    {
+      name: 'twitter:site',
+      content: '@manuelsandoble',
+    },
+    {
+      name: 'twitter:title',
+      content: 'Unofficial Capcom Advisor',
+    },
+    {
+      name: 'twitter:description',
+      content: 'Find out which game is for you with this cool advisor!',
+    },
+    {
+      name: 'twitter:image',
+      content: 'https://capcom.vercel.app/thumbnail.png',
     },
   ],
 }
