9784b0290f89d147e8caae17c7d25b8a0e69969b
manuelsanchez2 <manusansan22@gmail.com>
2023-06-23 15:23:53 +0200

    🔥 hotfix: Trying out

--- src/api/getGames.ts
+++ src/api/getGames.ts
@@ -2,12 +2,15 @@ import { type Game } from '~/types/types'
 
 export async function getGames(): Promise<Game[]> {
   const url = `${process.env.PUBLIC_APP_URL}data/data.json`
+  console.log('url', url)
   const res = await fetch(url)
+  console.log('res', res)
 
   if (!res.ok) {
     throw new Error('Error fetching games')
   }
 
   const data = (await res.json()) as Game[]
+  console.log('data', data)
   return data
 }
